03c6f333df762f1d2522fff0872fc8093045484a
ash8h <atsushi8h0617@hotmail.com>
2020-02-15 16:11:25 +0900

    - request CAMERA permission - load OpenCV libs

--- app/src/main/java/jp/ash8h/slam/MainActivity.kt
+++ app/src/main/java/jp/ash8h/slam/MainActivity.kt
@@ -1,12 +1,45 @@
 package jp.ash8h.slam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.RuntimePermissions
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        OpenCVLoader.initDebug()
+
+        initCameraWithPermissionCheck()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun initCamera() {
+
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun onCameraDenied() {
+        Toast.makeText(this, "CAMERA permission is required", Toast.LENGTH_SHORT).show()
+        setResult(Activity.RESULT_CANCELED)
+        finish()
     }
 }
